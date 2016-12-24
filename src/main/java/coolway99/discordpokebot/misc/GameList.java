5fff41120adbc3dbb108e18190bf4449ab67aa27
Coolway99 <xxcoolwayxx@gmail.com>
2016-12-24 10:09:31 -0500

    Updated to 1.2.3
    Updated Discord4J to 2.7.0 (Discord's API broke and therefore broke the
    bot.)
    Updated SparkJava to 2.5.4
    Added some teasers~
    Added some begging to Pokebot's help text. It's hard developing all of this by yourself...

--- src/main/java/coolway99/discordpokebot/misc/GameList.java
+++ src/main/java/coolway99/discordpokebot/misc/GameList.java
@@ -2,10 +2,10 @@ package coolway99.discordpokebot.misc;
 
 @SuppressWarnings("SpellCheckingInspection")
 public enum GameList{
-	POKEMON("Pokémon"),
+	/*POKEMON("Pokémon"),
 	PKMN("Pkmn"),
 	GO("Pokémon Go"),
-	GO2("our servers are down"),
+	//GO2("our servers are down"),
 	//GO3("Pokémon Go Fuck Yourself"),
 	POKERMON("some poker, mon"),
 	POKE_A_MON("poke a mon"),
@@ -20,13 +20,19 @@ public enum GameList{
 	XY("the ones we don't mention"),
 	ORAS("Omega Ruby/Alpha Saphire"),
 	//TODO DPPt remakes
-	SM("Sun/Moon")
+	SM("Sun/Moon")*/
+
+	DISCORD_BROKE_STUFF("discord broke everything, that's why the bot didn't work for a bit!"),
+	NEW_VERSION("overhaul in development!"),
+	TEASE("soon to have new move system where users can easily contribute moves? Sign me up."),
+	TEASE2("next update mmight have items~"),
+	PLEASE("please help with development! Contact Coolway#7694 for details"),
 	;
 	
 	private final String name;
 	
 	GameList(String name){
-		this.name= name;
+		this.name = name;
 	}
 	
 	public String getName(){
